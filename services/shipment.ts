51b47337dcedbf1c5d3daaad765834f5f4747242
ThankGod Ajayi <thankiziedon@gmail.com>
2021-11-16 13:49:35 +0530

    installed needed components : - react router - material - ui - axios

---
+++ services/shipment.ts
@@ -0,0 +1,8 @@
+/**
+ * This file handles getting & updating a shipment
+ * also persisting a shipment
+ */
+
+export const getShipment = async () => {};
+export const updateShipment = async () => {};
+export const retrieveFromCache = async () => {};
